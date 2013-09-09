baae37428fa63e31ba8572937f6173d46a1b3933
Luuk Verhoeven <Luuk Verhoeven@localhost>
2013-09-10 00:53:05 +0200

    Update: js Fix: some issues

---
+++ view/admin_dashboard.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * File: admin_dashboard.php
+ * Encoding: UTF-8
+ * @package: MOODLE PLUGINS
+ *
+ * @Version: 1.0.0
+ * @Since Sep 9, 2013
+ * @Author: Luuk Verhoeven
+ *
+ **/
---
+++ view/chat_teacher.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * File: chat_teacher.php
+ * Encoding: UTF-8
+ * @package: MOODLE PLUGINS
+ *
+ * @Version: 1.0.0
+ * @Since Sep 9, 2013
+ * @Author: Luuk Verhoeven
+ *
+ **/
