28d83af6d892934624c2d76821ddbafc6fa8233e
Luuk Verhoeven <Luuk Verhoeven@localhost>
2013-09-16 22:15:09 +0200

    Update: chatroom Add: avatars helper for gravatar

--- js/brigo_publicroom.js
+++ js/brigo_publicroom.js
@@ -1,7 +1,16 @@
 function resize()
 {
+    var wWidth = $(document).width();
     var wHeight = $(document).height();
+
+    $('#brigoRoomHolder').width(wWidth - 200);
+    $('#brigoUserHolder').width(200);
+
+    $('#brigoMessages ,#brigoMessages .viewport').height(wHeight - 196);
+    $('#brigoMessages .viewport').width(wWidth - 220);
     $('#brigoContent').height(wHeight - 50);
+    $('#brigoOnlineUsers .viewport,#brigoOnlineUsers').height(wHeight - 160);
+    $('#addBrigoMessage').width(wWidth - 200 - 95);
 }
 //SMART resizing
 (function($, sr) {
@@ -30,10 +39,99 @@ function resize()
     };
 })(jQuery, 'smartresize');
 
+function noHashSocket()
+{
+    if (typeof io === 'undefined')
+    {
+        log('Socket script not found! [server down or wrong url]');
+    }
+    console.log('Brigo doesn\'t have a license');
+}
+
+function send()
+{
+    var value = $.trim($('#addBrigoMessage').val());
+    var regex = /[*|,\\":<>\[\]{}`';()@&$#%!+-]/;
+    if (regex.test(value))
+    {
+        alert('Illegal characters used.');
+        return false;
+    }
+
+    $('#addBrigoMessage').val('');
+    if (value.length <= 0 || value.length > 400)
+    {
+        return false;
+    }
+    else if (client)
+    {
+        client.addMessage();
+        addRow(value , userid , username);
+    }
+}
+
+var avatars = [];
+function addRow(message, id, username)
+{
+    if (typeof avatars[id] === 'undefined')
+    {
+        $.ajax({
+            url: "/blocks/brigo/avatar.php?userid=" + id,
+            method: "get",
+            cache: false,
+            success: function(string) {
+                avatars[id] = string;
+                pushMessage(string, username, message);
+            }
+        });
+    }
+    else
+    {
+        pushMessage(avatars[id], username, message);
+    }
+}
+function pushMessage(avatar, username, message)
+{
+    var regex = /[*|,\\":<>\[\]{}`';()@&$#%!+-]/;
+    if (regex.test(message))
+    {
+        return;
+    }
+    var time = new Date();
+    time = time.getHours() + ':' + time.getMinutes();
+    $('#brigoMessages .overview').append('<div class="brigoRow"><div class="space"><div class="avatar"><img src="'+ avatar +'" alt="' + username + '"/></div><div class="text"><h5>' + username + ' - '+time+'</h5>' + message + '</div></div></div>');
+    $('#brigoMessages').tinyscrollbar_update('bottom');
+}
+
 !function($) {
 
     $(function() {
         resize();
+
+        $('#brigoSend').click(function(e){
+            e.preventDefault();
+            send();
+        });
+
+        $('#brigoOnlineUsers').tinyscrollbar();
+        $('#brigoMessages').tinyscrollbar();
+
+        if (typeof io !== 'undefined')
+        {
+            //config = $.parseJSON(config);
+            log(config);
+
+            client = brigo(server, {'username': username, 'hash': config.hash, 'id': userid, 'courseid': courseid});
+            client.joinRoom('publicRoom');
+
+            client.getClients('publicRoom');
+
+            client.getLastMessages('publicRoom', '#');
+        }
+        else
+        {
+            log('Socket script not found!');
+        }
     });
 
     $(window).focus(function() {
@@ -46,6 +144,12 @@ function resize()
 
     $(window).smartresize(function() {
         resize();
+    });
+
+    $(document).keypress(function(e) {
+        if (e.which == 13) {
+            send();
+        }
     });
 
 }(window.jQuery);
--- view/page/room.php
+++ view/page/room.php
@@ -9,10 +9,39 @@
 ?>
 <div id="brigoRoom">
     <div id="brigoRoomHolder">
-        <div id="brigoMessages"></div>
-        <input id="addBrigoMessage" type="text" name="text" value="" placeholder="<?php echo get_string('typehere' , Brigo_Config::NAME)?>"
+        <div class="space">
+            <h4><?php echo get_string('messages', Brigo_Config::NAME) ?></h4>
+            <div id="brigoMessages" class="brigoScroll">
+                 <div class="scrollbar"><div class="track"><div class="thumb"><div class="end"></div></div></div></div>
+                <div class="viewport">
+                    <div class="overview">
+                        <!-- users -->
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="input-group">
+                        <input id="addBrigoMessage" placeholder="<?php echo get_string('typehere', Brigo_Config::NAME) ?>" type="text" maxlength="400" class="form-control">
+                        <span class="input-group-btn">
+                            <button class="btn btn-default" type="button" id="brigoSend">Send</button>
+                        </span>
+                    </div><!-- /input-group -->
+                </div><!-- /.col-lg-6 -->
+            </div><!-- /.row -->
+        </div>
     </div>
     <div id="brigoUserHolder">
-        <div id="brigoOnlineUsers"></div>
+        <div class="space">
+            <h4 class="text-center"><?php echo get_string('online_clients', Brigo_Config::NAME) ?></h4>
+            <div id="brigoOnlineUsers" class="brigoScroll">
+                <div class="scrollbar"><div class="track"><div class="thumb"><div class="end"></div></div></div></div>
+                <div class="viewport">
+                    <div class="overview">
+                        <!-- users -->
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
